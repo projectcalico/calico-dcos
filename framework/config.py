1aaeead9eed964696339a64038f2234e0bec6c9e
Rob Brockbank <rob.brockbank@metaswitch.com>
2016-04-16 20:52:29 -0700

    Handle boolean config in environment variables

--- framework/config.py
+++ framework/config.py
@@ -11,18 +11,18 @@ class Config(object):
         self.calicoctl_url = self.getenv("CALICO_CALICOCTL_URL")
         self.node_img = self.getenv("CALICO_NODE_IMG")
         self.libnetwork_img = self.getenv("CALICO_LIBNETWORK_IMG")
-        self.allow_docker_update = self.getenv("CALICO_ALLOW_DOCKER_UPDATE")
-        self.allow_agent_update = self.getenv("CALICO_ALLOW_AGENT_UPDATE")
-        self.max_concurrent_restarts = self.getenv("CALICO_MAX_CONCURRENT_RESTARTS")
+        self.allow_docker_update = bool(self.getenv("CALICO_ALLOW_DOCKER_UPDATE", can_omit=True))
+        self.allow_agent_update = bool(self.getenv("CALICO_ALLOW_AGENT_UPDATE", can_omit=True))
+        self.max_concurrent_restarts = int(self.getenv("CALICO_MAX_CONCURRENT_RESTARTS"))
         self.zk_persist_url = self.getenv("CALICO_ZK")
         self.cpu_limit_install = float(self.getenv("CALICO_CPU_LIMIT_INSTALL"))
-        self.mem_limit_install = float(self.getenv("CALICO_MEM_LIMIT_INSTALL"))
+        self.mem_limit_install = int(self.getenv("CALICO_MEM_LIMIT_INSTALL"))
         self.cpu_limit_etcd_proxy = float(self.getenv("CALICO_CPU_LIMIT_ETCD_PROXY"))
-        self.mem_limit_etcd_proxy = float(self.getenv("CALICO_MEM_LIMIT_ETCD_PROXY"))
+        self.mem_limit_etcd_proxy = int(self.getenv("CALICO_MEM_LIMIT_ETCD_PROXY"))
         self.cpu_limit_node = float(self.getenv("CALICO_CPU_LIMIT_NODE"))
-        self.mem_limit_node = float(self.getenv("CALICO_MEM_LIMIT_NODE"))
+        self.mem_limit_node = int(self.getenv("CALICO_MEM_LIMIT_NODE"))
         self.cpu_limit_libnetwork = float(self.getenv("CALICO_CPU_LIMIT_LIBNETWORK"))
-        self.mem_limit_libnetwork = float(self.getenv("CALICO_MEM_LIMIT_LIBNETWORK"))
+        self.mem_limit_libnetwork = int(self.getenv("CALICO_MEM_LIMIT_LIBNETWORK"))
 
         self.installer_url = self.getenv("CALICO_INSTALLER_URL")
         self.calico_mesos_url = self.getenv("CALICO_MESOS_PLUGIN")
@@ -37,15 +37,21 @@ class Config(object):
         assert self.zk_persist_url.startswith("zk://")
         self.zk_hosts, self.zk_persist_dir = self.zk_persist_url[5:].split("/", 1)
 
+        # Trace out the environment variables.
+        for key, val in self.__dict__:
+            if key.startswith("_"):
+                continue
+            _log.debug("%s = %s", key, val)
+
         # If we are missing environment variables, trace them out now.
         if self._missing:
             # Print instead of logging, since logging hasn't been initialized
             print "Missing environment variables: %s" % self._missing
             sys.exit(1)
 
-    def getenv(self, key):
+    def getenv(self, key, can_omit=False):
         value = os.getenv(key)
-        if value is None:
+        if value is None and not can_omit:
             self._missing.append(key)
         return value
 
